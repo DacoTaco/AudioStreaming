fc7e00301e2298aa0ad8bf41a21acf166fecbbaa
DacoTaco <daco_65@hotmail.com>
2016-02-28 12:41:29 +0100

    moved the devices stuff to the main window. was a stupid move of mine to have it in server

--- AudioStreamingServer/MainWindow.xaml.cs
+++ AudioStreamingServer/MainWindow.xaml.cs
@@ -40,8 +40,10 @@ namespace AudioStreaming.Server
 
             Server = new audioServer();
             grdMain.DataContext = Server;
+            listDevices.DataContext = this;
+            GetDevices();
 
-            if (Server.GetDevicesCount() > 0 && listDevices.SelectedIndex < 0)
+            if (GetDevicesCount() > 0 && listDevices.SelectedIndex < 0)
             {
                 listDevices.SelectedIndex = 0;
             }
@@ -54,7 +56,7 @@ namespace AudioStreaming.Server
 
         private void btStart_Click(object sender, RoutedEventArgs e)
         {
-            if (Server.GetDevicesCount() > 0)
+            if (GetDevicesCount() > 0)
             {
                 Server.StartServer(listDevices.SelectedIndex);
 
@@ -86,6 +88,36 @@ namespace AudioStreaming.Server
         private void CloseApp(object sender, EventArgs e)
         {
             Server.KillThread();
+        }
+
+        /// <summary>
+        /// Gets all audio devices installed on the device and store them in 'Devices' which is data linked to the GUI
+        /// </summary>
+        private void GetDevices()
+        {
+            List<NAudio.Wave.WaveInCapabilities> devices = new List<NAudio.Wave.WaveInCapabilities>();
+
+            //gets the input (wavein) devices and adds them to the list
+            for (short i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                devices.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+            }
+
+            //claer the listview module
+            Devices = new List<Device>();
+
+            //each device gets inserted into the devices list, which is linked to the listdevices listview module
+            foreach (var device in devices)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Content = device.ProductName;
+                Devices.Add(new Device() { Device_name = device.ProductName, Channels = device.Channels });
+            }
+        }
+
+        public int GetDevicesCount()
+        {
+            return Devices.Count;
         }
     }
 }
--- AudioStreamingServer/Server/audioServer_General.cs
+++ AudioStreamingServer/Server/audioServer_General.cs
@@ -10,21 +10,8 @@ namespace AudioStreaming
     public partial class audioServer : NetworkBackend
     {
         //----------------------
-        //subclasses
-        //----------------------
-        /// <summary>
-        /// class to contain the device information
-        /// </summary>
-        public class Device
-        {
-            public string Device_name { get; set; }
-            public int Channels { get; set; }
-        }
-
-        //----------------------
         //variables
         //----------------------
-        public IList<Device> Devices { get; set; }
         private int deviceIndex = 0;
 
         private string _mp3Path;
@@ -66,40 +53,8 @@ namespace AudioStreaming
         public audioServer()
         {
             audioPlayer = new AudioRecorder();
-            GetDevices();
             return;
         }
-
-        /// <summary>
-        /// Gets all audio devices installed on the device and store them in 'Devices' which is data linked to the GUI
-        /// </summary>
-        private void GetDevices()
-        {
-            List<NAudio.Wave.WaveInCapabilities> devices = new List<NAudio.Wave.WaveInCapabilities>();
-
-            //gets the input (wavein) devices and adds them to the list
-            for (short i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
-            {
-                devices.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
-            }
-
-            //claer the listview module
-            Devices = new List<Device>();
-
-            //each device gets inserted into the devices list, which is linked to the listdevices listview module
-            foreach (var device in devices)
-            {
-                ListViewItem item = new ListViewItem();
-                item.Content = device.ProductName;
-                Devices.Add(new Device() { Device_name = device.ProductName, Channels = device.Channels });
-            }
-        }
-
-        public int GetDevicesCount()
-        {
-            return Devices.Count;
-        }
-
 
         private void GeneratePlayList()
         {
